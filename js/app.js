7edc73d8088d98c2418ef0528dd159167e642b22
Daniel Nunez <reachdanielnunez@gmail.com>
2016-07-02 10:14:12 -0500

    Added chiles.json, created table

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-angular.module('picosoApp', [])
-  .controller('FlavorController', function() {
+var picosoApp = angular.module('picosoApp', [])
+  picosoApp.controller('FlavorController', function() {
     var flavorGroup = this;
     flavorGroup.flavors = [];
 
     flavorGroup.pickFlavor = function(flavor) {
       flavorGroup.flavors.push(flavor);
     };
+  });
+
+  picosoApp.controller('ChileController', function($scope, $http) {
+    $http.get('js/chiles/chiles.json').success(function(data) {
+      $scope.chiles = data;
+    });
   });
